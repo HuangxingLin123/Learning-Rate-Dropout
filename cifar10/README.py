89a117f4d5076ac676328fe82f09f266c84cb8b1
HuangxingLin123 <58512753+HuangxingLin123@users.noreply.github.com>
2019-12-05 16:01:13 +0800

    Update README.py

--- cifar10/README.py
+++ cifar10/README.py
@@ -6,8 +6,8 @@ python main.py --model=resnet --optim=adam --lr=0.001
 
 python main.py --model=resnet --optim=sgd_lrd --lr=0.1 --LRD_p=0.5
 
-python main.py --model=resnet --optim=sgd --lr=0.1 --LRD_p=0.5
-
-python main.py --model=resnet --optim=rmsprop --lr=0.001
+python main.py --model=resnet --optim=sgd --lr=0.1 
 
 python main.py --model=resnet --optim=rmsprop_lrd --lr=0.001 --LRD_p=0.5
+
+python main.py --model=resnet --optim=rmsprop --lr=0.001
